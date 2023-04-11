cd0cbbe7a60fbecb3272c290760eaf2d5edb18c3
[Jonthan Gutierrez] <jonigutierrez741@gmail.com>
2023-04-11 01:20:57 -0400

    new class files added to the repo for week3

---
+++ Day1/04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,6 @@
+// Write Your JavaScript Code Here
+ var personName="Jonathan";
+ var numberOfPets= 2;
+ var funFact="this is an example";
+
+ console.log("hello my name is " + personName + " i have " + numberOfPets + " pets" );
